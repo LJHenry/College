f38154174d808c2ae84d62f2a39f0bb6308cd931
1406757 <1406757@Y350SDP08.ad.dundeeandangus.ac.uk>
2015-12-04 11:25:50 +0000

    numbers added to loop prints

---
+++ JavaApplication0014/src/javaapplication0014/JavaApplication0014.java
@@ -0,0 +1,13 @@
+package javaapplication0014;
+
+/**
+ *
+ * @author Louis Henry
+ */
+public class JavaApplication0014 {
+
+    public static void main(String[] args) {
+        // 
+    }
+    
+}
